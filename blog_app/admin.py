e44871d423d3e663946000f67280d2df32bba23c
vangola91 <aliksandrmax@yandex.ru>
2023-04-02 16:06:00 +0500

    create app blog models

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog_app.models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    
