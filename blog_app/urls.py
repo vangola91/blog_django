08b7afe40c681c66d47d9148a13ef2252b581683
vangola91 <aliksandrmax@yandex.ru>
2023-04-04 03:21:45 +0500

    Crete detail views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog_app.views import list_of_articles
+from blog_app.views import list_of_articles, article_details
 
 app_name = 'blog_app'
 
 urlpatterns = [
-    path( '' , list_of_articles, name= 'list_of_articles' ),
+    path('', list_of_articles, name= 'list_of_articles'),
+    path('<int:id>/', article_details, name='article_details'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from blog_app.models import Article
 
 
@@ -8,3 +8,10 @@ def list_of_articles(request):
     # print(articles)
 
     return render(request, 'blog/list.html', {'articles': articles})
+
+def article_details(request, id):
+    try:
+        article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
+    except Article.DoesNotExist:
+        raise Http404("No article found.")
+    return render(request, 'blog/detail.html', {'article': article})
